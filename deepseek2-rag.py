8fe0b1c17b440812cabbcd9599d4078d54c393b7
company-hary <godjaewook@naver.com>
2025-02-19 09:37:08 +0900

    fix embeddings

--- deepseek2-rag.py
+++ deepseek2-rag.py
@@ -5,7 +5,7 @@ from langchain_community.document_loaders import PDFPlumberLoader
 from langchain_experimental.text_splitter import SemanticChunker
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
-from langchain_ollama import OllamaLLM
+from langchain_ollama import OllamaLLM, OllamaEmbeddings
 from langchain.prompts import ChatPromptTemplate
 from langchain.chains.retrieval import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
@@ -70,26 +70,41 @@ st.title("DeepSeek R1(32B) RAG")
 # PDF 파일 업로드
 
 with st.sidebar:
-    if st.session_state['uploaded_file'] is False:
-        st.file_uploader("PDF 파일을 업로드하세요", type="pdf", key="file")
-
+    st.markdown("## 1.LLM 모델을 골라주세요.")
     model = st.selectbox(
-    "모델을 골라주세요.",
-    ("deepseek-r1:32b", "deepseek-r1:70b","sisaai/sisaai-llama3.1:latest", "benedict/linkbricks-llama3.1-korean:70b"),
+        label="## 1.LLM 모델을 골라주세요.",
+        label_visibility="collapsed",
+    options=["deepseek-r1:32b", "deepseek-r1:70b","sisaai/sisaai-llama3.1:latest", "benedict/linkbricks-llama3.1-korean:70b"],
 )
+    st.write("## 2.embedding 모델을 골라주세요.")
+    select_embedder = st.selectbox(
+    "2.embedding 모델을 골라주세요.",
+    label_visibility="collapsed",
+    options=("snowflake-arctic-embed2","HuggingFaceEmbeddings", "nomic-embed-text:latest"),
+)
+
+    if st.session_state['uploaded_file'] is False:
+        st.write("## 3.PDF 파일을 업로드하세요")
+        st.file_uploader("3.PDF 파일을 업로드하세요", type="pdf", key="file", label_visibility="collapsed")
 
 if st.session_state["file"] is not None:
     st.success("upload finished")
     uploaded_file = st.session_state["file"]
     # 파일명에서 확장자를 제외한 이름 추출
     file_name = Path(uploaded_file.name).stem
-    vector_store_dir = f"vector_stores/{file_name}"
+    vector_store_dir = f"vector_stores/{select_embedder}/{file_name}"
 
     # 벡터 스토어가 이미 존재하는지 확인
     if os.path.exists(vector_store_dir):
-        st.write("저장된 벡터 스토어를 불러옵니다.")
+
         # 기존 벡터 스토어 로드 - allow_dangerous_deserialization 파라미터 추가
-        embedder = HuggingFaceEmbeddings()
+        st.write("저장된 벡터 스토어를 불러옵니다.")
+
+        if select_embedder is "HuggingFaceEmbeddings":
+            embedder = HuggingFaceEmbeddings()
+        else:
+            embedder = OllamaEmbeddings(model=select_embedder)
+
         vector = FAISS.load_local(
             vector_store_dir, embedder, allow_dangerous_deserialization=True
         )
@@ -107,7 +122,12 @@ if st.session_state["file"] is not None:
         docs = loader.load()
 
         # 문서 분할기 초기화
-        embedder = HuggingFaceEmbeddings()
+        if select_embedder is "HuggingFaceEmbeddings":
+            embedder = HuggingFaceEmbeddings()
+        else:
+            embedder = OllamaEmbeddings(model=select_embedder)
+
+    
         text_splitter = SemanticChunker(embedder)
         documents = text_splitter.split_documents(docs)
 
