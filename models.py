c6be63a33d9edba4eb908acfa21e6bd2da16759c
company-hary <godjaewook@naver.com>
2025-02-21 11:26:27 +0900

    integration

---
+++ models.py
@@ -0,0 +1,10 @@
+LLM_options = [
+    "deepseek-r1:32b",
+    "sisaai/sisaai-llama3.1:latest",
+    "benedict/linkbricks-llama3.1-korean:70b",
+]
+EMBEDDING_options = [
+    "snowflake-arctic-embed2",
+    "HuggingFaceEmbeddings",
+    "nomic-embed-text:latest",
+]
