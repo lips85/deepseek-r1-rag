010112f26094bfa0ba42933cb47d75fd9cd22dcf
company-hary <godjaewook@naver.com>
2025-03-07 10:11:40 +0900

    model change

--- models.py
+++ models.py
@@ -1,7 +1,9 @@
 LLM_options = [
     "sisaai/sisaai-llama3.1:latest",
     "jinbora/deepseek-r1-Bllossom:8b",
+    "jinbora/deepseek-r1-Bllossom:70b",
     "deepseek-r1:32b",
+    "deepseek-r1:70b",
     "benedict/linkbricks-llama3.1-korean:70b",
 ]
 EMBEDDING_options = [
